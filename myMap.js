3cdb82ced4c14a0682ff97d2617307ac670a4272
Xingyao Huang <huangyy1995@gmail.com>
2016-12-27 12:00:39 -0500

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AjFkNBGu6xQ1QePUDJU7hVOegwUgCoLzqVMLgC8DhOHQHIVBp72ojtC3PAjUyR_U'
+    });
+}
