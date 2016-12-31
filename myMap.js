dbd5aa671a4566352ab27008a73ae2c4096fdb34
Xingyao Huang <huangyy1995@gmail.com>
2016-12-31 11:00:05 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -74,6 +74,6 @@ function refresh_pokemon_data() {
         console.log(result)
     });   
 }
-window.setInterval(refresh_pokemon_data, 10000);
+window.setInterval(refresh_pokemon_data, 5000);
 
-window.setInterval(refresh_pokemon_layer, 10000);
+window.setInterval(refresh_pokemon_layer, 5000);
