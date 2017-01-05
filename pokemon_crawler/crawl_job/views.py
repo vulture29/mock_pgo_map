e7afa75368e48aacbd4b8520dd8902363341cdd6
Xingyao Huang <huangyy1995@gmail.com>
2017-01-05 17:02:25 -0500

    Update views.py

--- pokemon_crawler/crawl_job/views.py
+++ pokemon_crawler/crawl_job/views.py
@@ -1,6 +1,7 @@
 import logging
 import json
 
+import redis
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -13,6 +14,15 @@ logger = logging.getLogger("worker")
 class Config:
     pass
 
+def needs_to_crawl(cell_id):
+    redis_client = redis.StrictRedis(host=os.environ["REDIS_HOST"], port=6379, db=0)
+    response = redis_client.get(cell_id)
+    if response == None:
+        redis_client.setex(cell_id, 30, "1")
+        return True
+    else:
+        return False
+
 def add_crawl_point(request):
     logger.info("I'm in add_crawl_point")
     # Crawl pokemon data
@@ -20,6 +30,10 @@ def add_crawl_point(request):
     # 1. Get cell id from request
     request_obj = json.loads(request.body)
     cell_id = request_obj["cell_id"]
+
+    # 1.1 Check if this cell id needs to be crawled
+    if not needs_to_crawl(cell_id):
+        return HttpResponse("Skip")
 
     # 2. Call my search api
     config = Config() 
